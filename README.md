4c3ef49669e30b03d99b398b478d46e03c0113bb
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-02-13 17:59:14 -0300

    Alterações realizadas dia 13/02/23

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Projeto DropBox Clone
+# Projeto DropBox Clone - PROJETO E README EM CONSTRUÇÃO
 
 Projeto desenvolvido como exemplo do Curso Completo de JavaScript na Udemy.com.
 
@@ -13,3 +13,7 @@ npm start
 ```
 
 http://localhost:3000
+
+
+-- Video para criar e implementar firebase
+https://www.youtube.com/watch?v=xJ6tEOcOBhY
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files) => {
+
+    res.json({
+      files,
+    });
+
+  });
+
+
 });
 
 module.exports = router;
